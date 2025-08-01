ec81102efc73c39baebd2ede46317e6ad331979c
Audi <monicauditya@gmail.com>
2025-08-01 16:15:15 +0530

    first commit

---
+++ src_code/Typesim(v1).py
@@ -0,0 +1,22 @@
+import pyautogui
+import random
+import time
+statements = [
+"""
+    "Hello everyone! How's it going?",
+    "Just wanted this to be autotyped",
+"""
+]
+print("Starting in 7 seconds... Switch to desired type area now!.")
+time.sleep(7)
+def type_and_send(statement):
+    pyautogui.typewrite(statement, interval=0.05)  # Type slower for realism
+    pyautogui.press('enter')  
+    time.sleep(1)
+while statements:
+    statement = random.choice(statements)
+    statements.remove(statement)
+    type_and_send(statement)
+    time.sleep(1) 
+
+print("✅ All statements Typed.")
